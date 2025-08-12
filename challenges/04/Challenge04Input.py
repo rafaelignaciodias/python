f6211774f52140565cb78191efb3738f8e7a7c52
Rafael Ignacio Dias <rafaelignaciodias@gmail.com>
2025-08-11 21:25:44 -0300

    Challege 04 exception update

--- challenges/04/Challenge04Input.py
+++ challenges/04/Challenge04Input.py
@@ -2,7 +2,11 @@
 # Asking name and age before showing these details
 
 first_name = input("What is your name? ")
-age = int(input("How old are you? "))
+try:
+    age = int(input("How old are you? "))
+except ValueError:
+    print("Please enter a valid number for age.")
+    exit()
 
 print(f"Hi, {first_name}! You are {age} years old.")
 print("Hi, {}! You are {} years old.".format(first_name, age))
