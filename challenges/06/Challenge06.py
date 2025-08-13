a2c5ccb5793b837898906cfc5c3227a1689456c4
Rafael Ignacio Dias <rafaelignaciodias@gmail.com>
2025-08-13 20:56:40 -0300

    Challenge 06 and 07

---
+++ challenges/06/Challenge06.py
@@ -0,0 +1,7 @@
+# Challenge 06
+# Defining and printing fruit list items
+
+fruit_list = ["apple", "banana", "mango", "grape"]
+
+for fruit in fruit_list:
+    print(fruit) 
---
+++ challenges/07/Challenge07.py
@@ -0,0 +1,7 @@
+# Challenge 07
+# Defining and printing the first and last list items
+
+fruit_list = ["apple", "banana", "mango", "grape"]
+
+print(fruit_list[0])
+print(fruit_list[-1])
