e1710ba6a14738d8ec348fd216873b6b7255a4db
Rafael Ignacio Dias <rafaelignaciodias@gmail.com>
2025-08-14 22:55:07 -0300

    Challenge 08 and 09

---
+++ challenges/08/Challenge08.py
@@ -0,0 +1,9 @@
+# Challenge 08
+# Defining, changing and adding items into a list
+
+fruit_list = ["apple", "banana", "mango", "grape"]
+fruit_list[1] = "strawberry"
+fruit_list.append("pineapple")
+fruit_list.extend(["passion fruit", "orange"])
+
+print(fruit_list)
---
+++ challenges/09/Challenge09.py
@@ -0,0 +1,10 @@
+# Challenge 09
+# Defining, adding and deleting items from a list
+
+fruit_list = ["apple", "banana", "mango", "grape"]
+fruit_list.append("pineapple")
+
+fruit_list.remove("mango")
+del fruit_list[-1]
+
+print(fruit_list)
