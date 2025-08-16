49c61e6e6c54b98947b539ec15de62cae9b97515
Rafael Ignacio Dias <rafaelignaciodias@gmail.com>
2025-08-15 23:18:27 -0300

    Challenge 10

---
+++ challenges/10/Challenge10.py
@@ -0,0 +1,8 @@
+# Challenge 10
+# Defining, reading abd printing all items from a list
+
+fruit_list = ["apple", "banana", "mango", "grape"]
+
+for fruit in fruit_list:
+    print(f"I like {fruit}.")
+
